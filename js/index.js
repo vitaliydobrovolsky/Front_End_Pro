e50988da04b1d2b85677dc57695eba9c58a574f2
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-18 20:00:14 +0000

    Update 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-const num = 10369;
+/*const num = 10369;
 const digits = num.toString().split('');
 const realDigits = digits.map(Number);
-console.log(realDigits);
 
+console.log(realDigits);*/
+
+
+const num = 10369;
+
+const x = num[0];
+const y = num[1];
+const z = num[2];
+const a = num[3];
+const b = num[4];
+
+const message = ` ${x} ${y} ${z} ${a} ${b}`;
