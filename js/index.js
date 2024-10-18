7aae710585719f9dc526331541e0dc8bb6e72d7b
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-18 19:32:18 +0000

    Created HomeWork 3.2

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const name = "Vitaliy";
+const age = 37;
+const placeOfBirth = "Ukraine";
+
+const message = `Hello, my name is ${name} and I am ${age} years old from ${placeOfBirth} .`;
+console.log(message);
