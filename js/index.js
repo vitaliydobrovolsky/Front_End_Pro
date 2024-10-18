c25c67f7ea7974445a2b524f5c8c5bf1e481db5c
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-18 18:30:29 +0000

    Lesson No 3, class work

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let user = "Bob";
+let admin = user;
+
+//primitive
+user = "John"
+
+
+//object
+user.name = "John";
+
+console.log(admin);
+
+const arr = [1, "jasd", "asdasd", true, user];
+
+console.log(arr[1]);
+
+console.log(length)
+
+console.log(typeof arr);
+
+console.log(NaN);
+
+let num = "42";
+
+console.log(+num);
+
+let test = !num;
+
+console.log(test);
+
+let user = {
+    name: 0;
+    job {
+        title: "Manager",
+    },
+}
+
+
+let x = Boolean(user.name);
+
