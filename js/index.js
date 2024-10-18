75b77a308ab6d348c15299e6a35c1d116bb9662e
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-18 20:57:52 +0000

    Complete Home Work 3.1

--- js/index.js
+++ js/index.js
@@ -1,19 +1,14 @@
 const num = 10369;
-const digits = num.toString().split('');
-const realDigits = digits.map(Number);
 
-console.log(realDigits);
+let text = num.toString();
 
-
-/*const num = 10369;
-
-let x = num[0];
-let y = num[1];
-let z = num[2];
-let a = num[3];
-let b = num[4];
+let x = text[0];
+let y = text[1];
+let z = text[2];
+let a = text[3];
+let b = text[4];
 
 let message = ` ${x} ${y} ${z} ${a} ${b}`;
-console.log(message);*/
+console.log(message);
 
 
