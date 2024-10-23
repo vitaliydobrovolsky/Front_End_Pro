b88babc1fb3e9a80640dc7379a2b932d03c54018
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-23 16:49:21 +0000

    amendments done, to fix remarks (1)

--- js/index.js
+++ js/index.js
@@ -1,33 +1,135 @@
 let age = prompt("How old are u?");
-if(age) {
-    alert(`You are ${age} old`);
-} else {
-    alert(`So sad that you would not to write your age`);
+let city = prompt("In which city are u living now?");
+let sport = prompt("What's your favourite kind of sport?");
+
+
+
+   if(age && city === "Kiyv" && sport === "football") {
+    alert(`You are ${age} years old, living in the capital of Ukraine & wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(age && city === "Washington" && sport === "football") {
+    alert(`You are ${age} years old, living in the capital of USA & wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(age && city === "London" && sport === "football") {
+    alert(`You are ${age} years old, living in the capital of GB & wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(age && city === "Kiyv" && sport === "big tennis") {
+    alert(`You are ${age} years old, living in the capital of USA & wanna be like Federer?`);
+    //console.log(message2);
+} if(age && city === "Washington" && sport === "big tennis") {
+    alert(`You are ${age} years old, living in the capital of USA & wanna be like Federer?`);
+    //console.log(message2);
+} if(age && city === "London" && sport === "big tennis") {
+    alert(`You are ${age} years old, living in the capital of GB & wanna be like Federer?`);
+    //console.log(message2);
+} if(age && city === "Kiyv" && sport === "box") {
+    alert(`You are ${age} years old, living in the capital of Ukraine & wanna be like Usik?`);
+    //console.log(message2);
+} if(age && city === "Washington" && sport === "box") {
+    alert(`You are ${age} years old, living in the capital of USA & wanna be like Usik?`);
+    //console.log(message2);
+} if(age && city === "London" && sport === "box") {
+    alert(`You are ${age} years old, living in the capital of GB & wanna be like Usik?`);
+    //console.log(message2);
+} if(age && city && sport === "football") {
+    alert(`You are ${age} years old, living in the ${city} & wanna be like Ronaldo?`);
+} if(age && city && sport === "big tennis") {
+    alert(`You are ${age} years old, living in the ${city} & wanna be like Federer?`);
+} if(age && city && sport === "box") {
+    alert(`You are ${age} years old, living in the ${city} & wanna be like Usik?`);
+} if(age && city === "Kiyv" && sport) {
+    alert(`You are ${age} years old, living in the capital of Ukraine & it is greate?`);
+} if(age && city === "Washington" && sport) {
+    alert(`You are ${age} years old, living in the capital of US & it is greate?`);
+} if(age && city === "London" && sport) {
+    alert(`You are ${age} years old, living in the capital of GB & it is greate?`);
+    
+
+
+
+}     if(age === "null" || city === "Kiyv" && sport === "football") {
+    alert(`So sad, you did not put your age, living in the capital of Ukraine & wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(age === "null" || city === "Washington" && sport === "football") {
+    alert(`So sad, you did not put your age, living in the capital of USA & wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(age === "null" || city === "London" && sport === "football") {
+    alert(`So sad, you did not put your age, living in the capital of GB & wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(age === "null" || city === "Kiyv" && sport === "big tennis") {
+    alert(`So sad, you did not put your age, living in the capital of USA & wanna be like Federer?`);
+    //console.log(message2);
+} if(age === "null" || city === "Washington" && sport === "big tennis") {
+    alert(`So sad, you did not put your age, living in the capital of USA & wanna be like Federer?`);
+    //console.log(message2);
+} if(age === "null" || city === "London" && sport === "big tennis") {
+    alert(`So sad, you did not put your age, living in the capital of GB & wanna be like Federer?`);
+    //console.log(message2);
+} if(age === "null" || city === "Kiyv" && sport === "box") {
+    alert(`So sad, you did not put your age, living in the capital of Ukraine & wanna be like Usik?`);
+    //console.log(message2);
+} if(age === "null" || city === "Washington" && sport === "box") {
+    alert(`So sad, you did not put your age, living in the capital of USA & wanna be like Usik?`);
+    //console.log(message2);
+} if(age === "null" || city === "London" && sport === "box") {
+    alert(`So sad, you did not put your age, living in the capital of GB & wanna be like Usik?`);
+    //console.log(message2);
+
+
+}   if(city === "Null" || age &&  sport === "football") {
+    alert(`You are ${age} years old. So sad, you did not put your city. Wanna be like Ronaldo?`);
+    //console.log(message2);
+} if(city === "Null" || age &&  sport === "big tennis") {
+    alert(`You are ${age} years old. So sad, you did not put your city. Wanna be like Federer?`);
+    //console.log(message2);
+}  if(city === "Null" || age && sport === "box") {
+    alert(`You are ${age} years old. So sad, you did not put your city. Wanna be like Usik?`);   
+} 
+
+
+   if(sport === "Null" || age &&  city === "Kiyv") {
+    alert(`You are ${age} years old & living in the capital of Ukraine. So sad, you did not put your favoutite sport`);
+    //console.log(message2);
+} if(sport === "Null" || age &&  city === "Washington") {
+    alert(`You are ${age} years old & living in the capital of USA. So sad, you did not put your favoutite sport`);
+    //console.log(message2);
+}  if(sport === "Null" || age && city === "London") {
+    alert(`You are ${age} years old & living in the capital of GB. So sad, you did not put your favoutite sport`);
+} 
+
+
+if(sport === "Null" && age === "Null" ||  city === "Kiyv") {
+    alert(`You are living in the capital of Ukraine. So sad, you did not put your age & favourite sport`);
+    
+} if(sport === "Null" && age === "Null" ||  city === "Washington") {
+    alert(`You are living in the capital of USA. So sad, you did not put your age & favourite sport`);
+    
+}  if(sport === "Null" && age === "Null" || city === "London") {
+    alert(`You are living in the capital of GB. So sad, you did not put your age & favourite sport`);
+} 
+
+
+if(sport === "Null" && city === "Null" ||  age) {
+    alert(`You are ${age} years old. So sad, you did not put your city & favourite sport`);
 }
 
 
-let city = prompt("In which city are u living now?");
-if(city === "Kiyv") {
-    alert(`You are living in the capital of Ukraine`);
-} if(city === "Washington") {
-    alert(`You are living in the capital of USA`);
-} if(city === "London") {
-    alert(`You are living in the capital of Greate Britain`);
-} if(city) {
-    alert(`You are living in ${city}`);
-} else {
-    alert(`So sad that you would not to write your city`);
-};
+if(city === "Null" && age === "Null" ||  sport === "football") {
+    alert(`So sad, you did not put your age & city. Wanna be like Ronaldo?`);
+    
+} if(city === "Null" && age === "Null" ||  sport === "big tennis") {
+    alert(`So sad, you did not put your age & city. Wanna be like Federer?`);
+    
+}  if(city === "Null" && age === "Null" || sport === "box") {
+    alert(`So sad, you did not put your age & city. Wanna be like Usik?`);
+} 
 
-let sport = prompt("What's your favourite kind of sport?");
-   if(sport === "football") {
-    alert(`Do you want to be like Ronaldo?`);
-} if(sport === "big tennis") {
-    alert(`Do you want to be like Federer?`);
-} if(sport === "box") {
-    alert(`Do you want to be like Usik?`);
-}  if(sport) {
-    alert(`It is greate!!`);
-} else {
-    alert(`So sad that you would not to write your sport`);
-};
+   else {
+    alert(`So sad, you did not input all your informatio, as age, city & favourite sport.`);
+} 
+
+
+
+
+
+
