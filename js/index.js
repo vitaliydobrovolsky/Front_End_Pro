049c29f063af74599b5c244ccaaac1240a0bf84f
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-23 06:32:34 +0000

    lesson 4 created

---
+++ js/index.js
@@ -0,0 +1,61 @@
+
+
+/*let year1 = prompt("Which year is now");
+
+let age = prompt("how old are you");
+
+let year2 = year1 - age;
+
+let message = `you was born on ${year2}`;
+
+alert(message);*/
+
+
+
+/*const x = null;
+const y = 'panda';
+const z = {};
+
+const foo =() =>{
+    console.log("FOO FUNC")
+    x = ''
+}
+
+const result = x ?? y ?? z ?? foo()
+console.log(result);*/
+
+
+
+
+/*let age = prompt("How old are u?"); 
+
+let message = age > 0 ? '1' : '-1';
+console.log(message);*/
+
+
+/*let a = 5;
+let b = 1;
+
+let result = a + b;
+
+let message = result < 4 ? 'Lower' : 'Upper';
+
+console.log(message);*/
+
+
+/*const data = 2;
+
+switch(data) {
+    case 1:
+        console.log('a');
+    break;
+    case 2:
+        console.log('b');
+    break;case 3:
+    console.log('c');
+     break;
+     default:
+    console.log('UA');
+
+}*/
+
