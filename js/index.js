03be4f5ac29c7b5bde4348538703e6212bdc0c98
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-23 08:57:25 +0000

    Completed Home Work 4.3

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let age = prompt("How old are u?");
+if(age) {
+    alert(`You are ${age} old`);
+} else {
+    alert(`So sad that you would not to write your age`);
+}
+
+
+let city = prompt("In which city are u living now?");
+if(city === "Kiyv") {
+    alert(`You are living in the capital of Ukraine`);
+} if(city === "Washington") {
+    alert(`You are living in the capital of USA`);
+} if(city === "London") {
+    alert(`You are living in the capital of Greate Britain`);
+} if(city) {
+    alert(`You are living in ${city}`);
+} else {
+    alert(`So sad that you would not to write your city`);
+};
+
+let sport = prompt("What's your favourite kind of sport?");
+   if(sport === "football") {
+    alert(`Do you want to be like Ronaldo?`);
+} if(sport === "big tennis") {
+    alert(`Do you want to be like Federer?`);
+} if(sport === "box") {
+    alert(`Do you want to be like Usik?`);
+}  if(sport) {
+    alert(`It is greate!!`);
+} else {
+    alert(`So sad that you would not to write your sport`);
+};
