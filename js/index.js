10db6ab25a4442e3383f2b69f396617e58fc49ce
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-23 07:07:28 +0000

    completed HW 4.2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 let num = prompt("Write here any number with digits")
-
 let x = num[0];
 let y = num[1];
 let z = num[2];
+let result1 = x === y && x === z && z === y;
+console.log(result1);
+
+let result2 = x === y || x === z || z === y;
+console.log(result2);
