ad7bb1f2d5159dde9d13128ef39c0aa02ceb3fa8
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-25 16:12:24 +0000

    3rd change

--- js/index.js
+++ js/index.js
@@ -6,15 +6,33 @@ console.log(numOrStr)
 switch(numOrStr) {
     case null:
         console.log('ви скасували');
+        console.log(typeof numOrStr);
     break;
     case '':
         console.log('Empty String');
+        console.log(typeof numOrStr);
     break;
-    case  isNaN(+numOrStr):
+    case  isNaN(numOrStr):
     console.log('number is Ba_NaN');
+    console.log(typeof numOrStr);
      break;
      default:
     console.log('OK!');
+    console.log(typeof numOrStr);
 }
 
 
+
+/*if(numOrStr === null) {
+	console.log('ви скасували')
+    console.log(typeof numOrStr);
+} else if( numOrStr.trim() === '' ) {
+	console.log('Empty String');
+    console.log(typeof numOrStr);
+} else if ( isNaN( +numOrStr ) ) {
+	console.log(' number is Ba_NaN')
+    console.log(typeof numOrStr);
+} else {
+	console.log('OK!')
+    console.log(typeof numOrStr);
+}*/
