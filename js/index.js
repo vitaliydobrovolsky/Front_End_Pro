1ae58b0c560de899f08754f0dc28cf3c2dc22ec0
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-25 17:09:39 +0000

    lesson 5

---
+++ js/index.js
@@ -0,0 +1,42 @@
+
+//while
+/*let i = 0;
+while ( i < 3) {
+    alert(i);
+    i++;
+}*/
+
+
+//do...while
+/*let i = 0;
+do {
+    alert(i);
+    i++;
+} while ( i < 3);*/
+
+
+//for
+/*for (let i = 0; i < 3; i++) {
+    alert(i);
+}*/
+
+
+//practice
+/*let i = 1;
+while ( i < 10) {
+    alert(i*i);
+    i++;
+}
+
+let i = 1;
+do {
+    alert(i*i);
+    i++;
+} while ( i < 10);
+
+for (let i = 1; i < 10; i*i) {
+    alert(i);
+}*/
+
+
+
