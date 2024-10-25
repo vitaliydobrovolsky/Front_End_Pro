ff484f813f8ffbc86e3969d3d9de58a89d20980d
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-25 17:51:09 +0000

    done

--- js/index.js
+++ js/index.js
@@ -38,5 +38,41 @@ for (let i = 1; i < 10; i*i) {
     alert(i);
 }*/
 
+/*let result = 0;
+for(let i = 1; i <500; i++) {
+    result += i;
+}
+console.log(result/500)*/
 
 
+/*let sum = 0;
+for(let i = 100; i <= 200; i++) {
+    if(i % 3 === 0) {
+        sum +=i;
+    }
+}
+console.log(+sum)*/
+
+
+
+//proizvedenie kratnyh 4isel
+/*for (let i=100; i<=200; i++){
+    if (i%3 === 0) {
+        console.log(i);
+    }
+}*/
+
+//tablica umnogeniya
+for (let i = 2; i <= 10; i++) {
+
+    console.log(`Таблица умножения ${i}:`);
+
+    for (let j = 1; j <= 10; j++) {
+
+        console.log(`${i} x ${j} = ${i * j}`);
+
+    }
+
+    console.log('');  
+
+}
