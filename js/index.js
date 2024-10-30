82915761becb22323bb24c55b51a33ba69e7c283
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-30 14:45:10 +0000

    2nd update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,28 @@
 let array = ["hello", 12, "/", 23, 1, "pp", "buy", 52, 41, 2, "!!"];
-let finalArray = '';
+
+let finalArray = 0;
+let element = 0;
+let array2 = [];
 
 function arithMean() {
     for(i = 0; i < array.length; i++) {
-        if(array[i] >= 0) {
+        if(+array[i]) {
            let n = +array[i];
+          
             finalArray += n;
-            console.log(finalArray);
-            console.log(i);
-        }
+            array2.push([n]);
+            console.log(finalArray)
+        }    
     }
-    console.log(finalArray);
+    //console.log(finalArray);
+    //alert(finalArray)
+    //alert(array2)
     
+   
+   //console.log(array2.length)
+   
+   result = finalArray / array2.length;
+   alert(result)
 }
 
 arithMean();
