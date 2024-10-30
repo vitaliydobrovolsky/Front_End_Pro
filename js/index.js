6c8c82f1565fe56ba2472dc444b6a11900aed27c
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-10-30 06:42:58 +0000

    Lesson 6 complete

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/*let x = 1;
+let y = 1;
+
+let test = function( ) {
+     z = 2; 
+     let x = 20; 
+    console.log(x)
+}
+
+
+console.log(x)
+test()
+
+let user = {}*/
+
+/*let x = 2;
+let y = 5;
+
+function min(x, y) {
+    let result = x < y ? x : y;
+        console.log(result) ; 
+}*/
+
+/*const x = +prompt('x:');
+const n = +prompt('n:');
+function pow(x, n){
+
+    let result = x;
+
+    for (let i=1; i<n; i++){
+        result *= n;
+    }
+    alert(result);
+}
+pow(x, n);*/
