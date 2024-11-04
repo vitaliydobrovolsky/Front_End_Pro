76b3b9655aa020dc5f7a4dabc2235df98dcaeda6
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-04 13:25:59 +0000

    4th update

--- js/index.js
+++ js/index.js
@@ -7,17 +7,18 @@ for(i = 0; i < 9; i++ ) {
    
        if (info <= 100) { 
         info = prompt(`Please enter number more than 100`);
-       } else if (isNaN(info)) {
-       // console.log(`You entered ${info}`); break;
-       // alert(`You entered ${info}`); break;
        } else if (info > 100) {
+        console.log(typeof info)
         //console.log(`You entered ${info}`); break;
       //  alert(`You entered ${info}`); break;
-       } 
-       
-        } 
+       } else  {
+        console.log(typeof info )
+       // console.log(`You entered ${info}`); break;
+       // alert(`You entered ${info}`); break;
+       }        
+        }      
    console.log(`You entered ${info}`)
-   alert(`You entered ${info}`); 
+   alert(`You entered "${info}"`); 
   
      } 
 
