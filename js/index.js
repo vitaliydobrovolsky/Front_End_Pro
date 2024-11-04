5f5a54cbc00d942ae896d6224f23f8b0857461e0
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-04 09:57:28 +0000

    2nd update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 
-
 function expr(info) {
-
-
 
   info = prompt(`Please enter number more than 100`); 
 
@@ -10,27 +7,20 @@ for(i = 0; i < 9; i++ ) {
    
        if (info <= 100 &&  Number) { 
         info = prompt(`Please enter number more than 100`);
-       } else if (info !== Number) {
-        console.log(`You entered ${info}`); break;
-        alert(`You entered ${info}`); break;
+       } else if (typeof info !== Number) {
+       // console.log(`You entered ${info}`); break;
+       // alert(`You entered ${info}`); break;
        } else if (info > 100 &&  Number ) {
-        console.log(`You entered ${info}`)
-        alert(`You entered ${info}`);
-       } else if (i = 3) {
-        console.log(`You entered ${info}`)
-        alert(`You entered ${info}`);
-       }
+        //console.log(`You entered ${info}`); break;
+      //  alert(`You entered ${info}`); break;
+       } 
        
         } 
    console.log(`You entered ${info}`)
    alert(`You entered ${info}`); 
-   
+  
      } 
 
-
 let value = expr()
-if (value <= 100 &&  Number) {
-    alert(value)
-}
 
 
