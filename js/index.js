350bba99129558df27a9958b6a9c167d1af92d9b
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-06 14:32:55 +0000

    creation of HW 8.1

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let ladder = {
+    up: function () { // підніматиме вас на одну сходинку
+    },
+    down: function () { // опускатиме вас на одну сходинку
+    },
+    showStep: function () { // показує поточну сходинку
+    }
+  };
+
+  ladder.showStep()
+
+  console.log(showStep)
