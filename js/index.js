3b378f5ddd2a260b714d240b5bfcec083ad141d0
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-08 19:13:44 +0000

    creation of HM 9.1

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let company = {
+    sales: [{name: 'John', slary: 1000}, {{name: 'Alica', slary: 600}}],
+    development: {
+       web: [{name: 'Peter', slary: 2000}, {{name: 'Alex', slary: 1600}}],
+       internals:  [{name: 'Jack', slary: 1000}]
+}
+}
