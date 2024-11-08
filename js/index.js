b97612fa44d3414b62a3ec434a8e5fade645f710
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-08 19:07:57 +0000

    Lesson 9

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/*function foo(n) {
+   if (n===0) {
+    return 1
+   }
+
+   return n * foo(n-1)
+}
+
+console.log(foo(5))*/
+
+
+/*function Car(model, year) {
+    this.model = model,
+    this.year = year,
+
+    this.getModel = function() {
+        return this.model;
+    }
+
+    this.setModel = function() {
+        if(model) {
+        this.model = model;
+    }
+}
+}
+const car = new Car('Opel', 2014)
+
+console.log(car)*/
+
+
+
+    
+     
+    
+
