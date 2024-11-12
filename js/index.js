b70d68334b1ff72f7dd19a18d56a5d0be8dad5c5
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-12 14:13:28 +0000

    last update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,50 @@
 let company = {
-    sales: [{name: 'John', slary: 1000}, {name: 'Alica', slary: 600}],
-    development: {
-       web: [{name: 'Peter', slary: 2000}, {name: 'Alex', slary: 1600}],
-       internals:  [{name: 'Jack', slary: 1000}]
-}
+     sales: [{name: 'John', salary: 1000}, {name: 'Alica', salary: 600}],
+     development: {
+       web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1600}],
+       internals:  [{name: 'Jack', salary: 1000}]
+}   
 };
+
+
+function foo(company) {
+    
+        
+      
+     } for (i=0; i<Object.keys(company).length; i++) {
+        let result = 0; 
+        if (i>Object.keys(company)[i].length) {
+             result;
+        } else if (Array(Object.keys(company)[i]) !== Array) {
+             result += Array(Object.keys(company)[i].salary)   ;
+    }  else { 
+           Object.keys(company)[i+1]
+
+        }      
+        console.log(result)   
+}
+
+ 
+//console.log(Array.isArray(company[0]))
+console.log(Object.keys(company)[0].length)
+
+console.log(Object.keys(company)[1])
+
+
+/*let common = [];
+common.push(...company.sales,...company.development.web,...company.development.internals)
+//console.log(common)
+//console.log(common[3].salary)
+//console.log(common.length)
+let result = 0;
+for(let i = 0; i < common.length; i++) {
+     
+     if(i < common.length) {
+       result += common[i].salary  
+     }
+     // console.log(common[i].salary) 
+     // console.log(result)
+}
+console.log(result)*/
+  
+
