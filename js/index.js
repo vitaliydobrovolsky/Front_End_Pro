68a7512d035f2b173210c46cd691b6fbc9807992
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-13 03:51:13 +0000

    creation of HW 10.1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const person = {
+    firstName: 'Carl',
+    lastName: 'Fin',
+    age: 30,
+    livingPlace: 'London'
+  };
+
+  console.log('user:' + person.firstName, person.lastName, person.age + 'years old', 'live in:' + person.livingPlace); 
