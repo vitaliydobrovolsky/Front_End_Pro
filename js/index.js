44edc67dc9079d85e635e87942bf20bd89014072
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-13 09:31:30 +0000

    2nd update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
-const person = {
+const user = {
+    id: 1,
     firstName: 'Carl',
     lastName: 'Fin',
     age: 30,
-    livingPlace: 'London'
+    livingPlace: 'London',
+      sayPersonInfo() {
+        console.log(user)
+      }
   };
+user.sayPersonInfo()
 
-  //console.log('user:' + person.firstName, person.lastName, person.age + 'years old', 'live in:' + person.livingPlace); 
 
+ // const person1 = Object.create(person);  
+  //console.log(person1)
 
-  const userInfo = Object.entries(person);
-  console.log(userInfo)
+  /*const personObj = Object.entries(person).reduce((acc, [key, value]) => {
+    return {...acc, [value.id]: value}
+  }, {});
+
+  console.log(personObj)*/
