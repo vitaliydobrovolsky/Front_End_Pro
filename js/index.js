6b87ec2dfc8f34a5402bdf65b285f8a270279b85
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-13 09:57:20 +0000

    3rd update

--- js/index.js
+++ js/index.js
@@ -6,22 +6,38 @@ const personCard = {
   };
 
 
-  const contactsNote = {
+  const contacts = {
     1: {name: 'Debora', familyName: '', phoneNumber: '+12358795', email: '18978@456.com'},
-    2: {name: 'Simon', familyName: '', phoneNumber: '++56897851', email: '6598@456.com'}
+    2: {name: 'Simon', familyName: '', phoneNumber: '++56897851', email: '6598@456.com'},
+    findPerson(){
+        const contactsList = Object.values(contacts)
+        let surchName = prompt(`Contact Name`)
+        const userName = contactsList.filter(contacts => contacts.name === surchName)
+        console.log(userName)        
+    }, 
+    addPerson (obj) {
+        const contactsList = Object.values(contacts)
+        const personCard1 = Object.values(obj)
+        contactsList.push(personCard)
+        console.log(contactsList)
+
+    } 
+    
 }
-  
+ 
+  contacts.findPerson();
+  contacts.addPerson(personCard)
 
   //console.log(contactsNote)
-  const contactsList = Object.values(contactsNote)
+  /*const contactsList = Object.values(contactsNote)
   console.log(contactsList)
   let surchName = prompt(`Contact Name`)
-  const userName = contactsList.filter(contacts => contacts.name === surchName)
+  const userName = contactsList.filter(contacts => contacts.name === surchName)*/
 
   
   //const personCard1 = Object.values(personCard)
  // console.log(personCard1)
-  contactsList.push(personCard);
-  console.log(contactsList)
+ //contactsList.push(personCard);
+ // console.log(contactsList)
 
  
