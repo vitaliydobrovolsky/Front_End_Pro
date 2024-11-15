f8b715c6170e2fef9a60cb605e06034f602ddf49
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-15 19:03:47 +0000

    completed

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/*let elem1 = document.getElementsByClassName("list")
+console.log(elem1)
+
+
+const elem2 = document.getElementsByTagName("li")
+
+const second = elem2[1];
+second.textContent = "Pix"
+
+
+let elem3 = document.getElementsByTagName("div") 
+//elem3.style.color = 'blue'
+
+console.log(elem3)
+
+const clickButton = document.getElementById('btn');
+function text() {
+    alert('Hello')
+    console.log('text')
+}
+
+clickButton.addEventListener('click', text);*/
+
+let elem1 = document.getElementsByClassName(".link1")
+const burgermenue =  document.getElementById('btnmenue')
+function bMenue() {
+    
+}
+
+elem1.style.display = "none";
+clickButton.addEventListener('click', bMenue);  
+
+
+clickButton.removeEventListener('click', text);
