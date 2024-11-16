ecdae114b7192b109ea75f8d3bfd62a53dde59f1
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-16 07:02:37 +0000

    HW 11.2 completed

--- js/index.js
+++ js/index.js
@@ -2,11 +2,12 @@ const elText= document.getElementById('text')
 const elButton = document.getElementById('btn')
 elText.style.color = 'blue'
 
-elButton.addEventListener('click', (event) => {
-    if(event.elText.style.color === 'blue') {
-        event.elText.style.color === 'red'
-    } else {
-        event.elText.style.fontColor === 'blue'
-        console.log('elButton clicked')
-    }
-} )
+
+
+    elButton.addEventListener('click', function(event) {
+        if(elText.style.color === 'blue') {
+            elText.style.color = 'red'
+        } else if (elText.style.color === 'red'){
+            elText.style.color = 'blue'
+        }
+    });
