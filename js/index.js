262bf16f92c39ff051ae6e442abc1b5785303e1c
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-11-22 04:52:12 +0000

    3rd update

--- js/index.js
+++ js/index.js
@@ -5,38 +5,62 @@ const ul = document.getElementById('ul')
 const input = document.getElementById('mytask');
 const li = document.getElementsByTagName('li')
 
+
 addElem.addEventListener('click', (event) => {
     const li = document.createElement("li");
     const newButton = document.createElement('button')
     newButton.textContent = 'Delete'
+    const val1 = 'ul_li'
+    li.setAttribute('id', val1)
     li.appendChild(document.createTextNode(input.value));
+    const val2 = 'deleteNode(this)'
+    newButton.setAttribute('onclick', val2)
     ul.appendChild(li);
     li.appendChild(newButton)
 });
 
 
-parent.addEventListener('click', (event) => {
-    if( event.target.textContent === 'Delete' ) {
-        for (let i = 0; i <li.length; i++){
-             li[i].querySelector("button").addEventListener("click",
-             function(){
+function deleteNode(item){
+    item.parentNode.remove()
+}
+
+
+
+
+
+
+//parent.addEventListener('click', (event) => {
+  //if( event.target.textContent === 'Delete' ) {
+  //     for (let i = 0; i <li.length; i++){
+             /*li[i].querySelector("button").addEventListener("click", function(){
              this.closest("li").remove();
-             });             
-           }
-    }
-    console.log(event.target.textContent)
-});
+             });        */
+            /* if (ul.hasChildNodes()) {
+                ul.removeChild(ul.children[i]);
+              }   break;*/
+             
+      //     } 
+      //     console.log(li.length)
+         
+   // console.log(event.target.textContent)
+   //    }
+    
+//});
 
 
 
 
-  /* for (let i = 0; i <li.length; i++){
-     /* li[i].addEventListener("click", function(){
+
+
+
+
+
+  /*for (let i = 0; i <li.length; i++){
+      li[i].addEventListener("click", function(){
          this.classList.toggle("active");
-      });*/
-    /*  li[i].querySelector("button").addEventListener("click",
-      function(){
+      });
+    li[i].querySelector("button").addEventListener("click", function(){
          this.closest("li").remove();
       });
-      
     }*/
+   
