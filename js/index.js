3b9498549e883b26011eb7387f85ed8d906d2d55
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-12-05 16:00:47 +0000

    2nd update - compelted

--- js/index.js
+++ js/index.js
@@ -1,78 +1,112 @@
 
+const todoForm = document.querySelector('.todo-form');
 
+const todoInput = document.querySelector('.todo-input');
 
-const inputText = document.getElementById('todo-item__description')
+const todoItemsList = document.querySelector('.todo-items');
 
-const items = JSON.parse(localStorage.getItem("js--todos-wrapper")) || [];  
-function addTodo() {
-    var inputBox =  document.getElementById("js--form__input");
-    var item = inputBox.value;
+let todos = [];
+
+todoForm.addEventListener('submit', function(event) {
+  
+  event.preventDefault();
+  addTodo(todoInput.value); 
+});
+
+function addTodo(item) {
+  
+  if (item !== '') {
     
-    if (item === "")
-      return (document.getElementById("js--form__input").innerHTML =
-        "You need to put in a number");
-    items.push({
-      value: item,
-    });
-    
-    localStorage.setItem("js--todos-wrapper", JSON.stringify(items));
-    newElement();
-   
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false
+    };
+
+    todos.push(todo);
+    addToLocalStorage(todos);
+
+    todoInput.value = '';
   }
-
-
-const form = document.querySelector('form');
-form.addEventListener('submit', event => {
-    event.preventDefault()
-} )
-
-function newElement() {
-  
-  const li = document.createElement("li");
-  const inputValue = document.getElementById("js--form__input").value;
-  const t = document.createElement('span');
-  const inputBox = document.getElementById("js--form__input");
-  var item = inputBox.value;
-  t.id = "todo-item__description";
-  const inputT = document.createTextNode(inputValue)
-  t.appendChild(inputT)
-  li.className = "todo-item";
-  const checkBox = document.createElement("input");
-  checkBox.type = "checkbox";
-  li.appendChild(checkBox);
-  li.appendChild(t);
-  
-    const btn = document.createElement("button");
-    const txt = document.createTextNode("Delete");
-    btn.className = "todo-item__delete";
-    btn.appendChild(txt);
-    li.appendChild(btn);
-    document.getElementById("js--todos-wrapper").appendChild(li);
-    
-   
-  
-  /*if (inputValue === '') {
-    return false
-  } else {
-    items.push({
-        value: item,
-      });
-    document.getElementById("js--todos-wrapper").appendChild(li);
-  }*/
-  document.getElementById("js--form__input").value = "";
-
-  const close = document.getElementsByClassName("todo-item__delete");
-  for (i = 0; i <= close.length; i++) {
-    close[i].onclick = function() {
-      const div = this.parentElement;
-      div.style.display = "none";
-    }
-   
-  } 
-  
 }
 
+function renderTodos(todos) {
+  
+  todoItemsList.innerHTML = '';
+
+  todos.forEach(function(item) {
+   
+    const checked = item.completed ? 'checked': null;
+
     
+    const li = document.createElement('li');
+   
+    li.setAttribute('class', 'item');
+   
+    li.setAttribute('data-key', item.id);
+    
+    if (item.completed === true) {
+      li.classList.add('checked');
+    }
+li.innerHTML = `
+      <input type="checkbox" class="checkbox" ${checked}>
+      ${item.name}
+      <button class="delete-button">Видалити</button>
+    `;
+    
+    todoItemsList.append(li);
+  });
+}
 
+function addToLocalStorage(todos) {
 
-  console.log(items)
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  renderTodos(todos);
+}
+
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+
+  if (reference) {
+   
+    todos = JSON.parse(reference);
+    renderTodos(todos);
+  }
+}
+
+function toggle(id) {
+  todos.forEach(function(item) {
+    
+    if (item.id == id) {
+     
+      item.completed = !item.completed;
+    }
+  });
+addToLocalStorage(todos);
+}
+
+function deleteTodo(id) {
+
+  todos = todos.filter(function(item) {
+    
+    return item.id != id;
+  });
+
+  addToLocalStorage(todos);
+}
+
+getFromLocalStorage();
+
+todoItemsList.addEventListener('click', function(event) {
+  
+  if (event.target.type === 'checkbox') {
+  
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+
+  if (event.target.classList.contains('delete-button')) {
+   
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
