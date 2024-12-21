6ffa0012f88379cf02b7557732108872e185c0ec
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-12-21 09:52:32 +0000

    4

--- js/index.js
+++ js/index.js
@@ -9,23 +9,39 @@ const li = document.getElementsByTagName('li')
 addElem.addEventListener('click', (event) => {
     const li = document.createElement("li");
     const newButton = document.createElement('button')
+    const par = document.createElement('p')
     newButton.textContent = 'Delete'
     const val1 = 'ul_li'
     li.setAttribute('id', val1)
-    li.appendChild(document.createTextNode(input.value));
+    par.appendChild(document.createTextNode(input.value));
     const val2 = 'deleteNode(this)'
     newButton.setAttribute('onclick', val2)
+    const val3 = 'showModal()'
+    par.setAttribute('onclick', val3)
     ul.appendChild(li);
+    li.appendChild(par)
     li.appendChild(newButton)
+    
 });
-
-
 function deleteNode(item){
     item.parentNode.remove()
 }
 
+
+
+let modal = new bootstrap.Modal(document.getElementById('gfg'));
+function showModal() {
+modal.show();
+
+}
+
+
+
+
 $(document).ready(function(){
-    $(".ul_li")
+    $("#ul_li").click(function(){
+        
+    })
 })
 
 
