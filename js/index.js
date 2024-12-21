0c8859a7043439920dc0982906fcdf6224033217
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-12-21 15:48:37 +0000

    completed

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    
+    $('#addTaskBtn').click(function () {
+        const taskText = $('#taskInput').val().trim();
+        if (taskText) {
+            $('#taskList').append(
+                `<li class="list-group-item d-flex justify-content-between align-items-center" data-task="${taskText}">
+                   <span> ${taskText}</span>
+                    <button class="btn btn-danger btn-sm deleteTaskBtn">Видалити</button>
+                </li>`
+            );
+            $('#taskInput').val(''); 
+        }
+    });
+
+    
+    $('#taskList').on('click', '.deleteTaskBtn', function () {
+        $(this).closest('li').remove(); 
+    });
+
+    
+    $('#taskList').on('click', 'span', function () {
+        const taskContent = $(this).data('task');
+        $('#modalTaskContent').text(taskContent); 
+        $('#taskModal').modal('show'); 
+    });
+});
