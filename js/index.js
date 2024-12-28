48d9c7909e6204412159c40a5206a45720d95a46
vitaliydobrovolsky <vitaliydobrovolsky@gmail.com>
2024-12-28 16:14:32 +0000

    optimization

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
 
     
     const addTaskBtn = document.querySelector('#addTaskBtn');
@@ -32,4 +32,25 @@ document.addEventListener('DOMContentLoaded', () => {
             $('#taskModal').modal('show');
         }
     });
-});
+});*/
+
+
+
+
+$(document).ready(function () {
+    $('#addTaskBtn').click(function () {
+      var taskText = $('#taskInput').val().trim();
+      if (taskText) {
+        $('#taskList').append("<li class=\"list-group-item d-flex justify-content-between align-items-center\" >\n                   <span data-task=\"".concat(taskText, "\">").concat(taskText, "</span>\n                    <button class=\"btn btn-danger btn-sm deleteTaskBtn\">\u0412\u0438\u0434\u0430\u043B\u0438\u0442\u0438</button>\n                </li>"));
+        $('#taskInput').val('');
+      }
+    });
+    $('#taskList').on('click', '.deleteTaskBtn', function () {
+      $(this).closest('li').remove();
+    });
+    $('#taskList').on('click', 'span', function () {
+      var taskContent = $(this).data('task');
+      $('#modalTaskContent').text(taskContent);
+      $('#taskModal').modal('show');
+    });
+  });
