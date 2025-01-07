cc25cf139cc86a6ea921e2fe2f6f97953f381aa5
vitaliydobrovolsky <vitaliydobrovolsky986@gmail.com>
2025-01-07 21:24:45 +0200

    completed

--- Front_End_Pro/gulpfile.js
+++ Front_End_Pro/gulpfile.js
@@ -1,25 +1,31 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));  // Using the dart-sass compiler
+const sass = require('gulp-sass')(require('sass'));  
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 
-// SCSS Compilation and Minification Task
+
 gulp.task('styles', () => {
-  return gulp.src('src/scss/**/*.scss')  // Input directory for SCSS files
-    .pipe(sass().on('error', sass.logError)) // Compile SCSS
-    .pipe(cleanCSS()) // Minify CSS
-    .pipe(rename({ suffix: '.min' })) // Rename to add .min
-    .pipe(gulp.dest('dist/css')); // Output directory for compiled CSS
+  return gulp.src('src/scss/**/*.scss')  
+    .pipe(sass().on('error', sass.logError)) 
+    .pipe(cleanCSS()) 
+    .pipe(rename({ suffix: '.min' })) 
+    .pipe(gulp.dest('dist/css')); 
 });
 
-// JavaScript Minification Task
+
 gulp.task('scripts', () => {
-  return gulp.src('src/js/**/*.js')  // Input directory for JS files
-    .pipe(uglify()) // Minify JS
-    .pipe(rename({ suffix: '.min' })) // Rename to add .min
-    .pipe(gulp.dest('dist/js')); // Output directory for minified JS
+  return gulp.src('src/js/**/*.js')  
+    .pipe(uglify()) 
+    .pipe(rename({ suffix: '.min' })) 
+    .pipe(gulp.dest('dist/js')); 
 });
 
-// Default task to run both 'styles' and 'scripts'
+gulp.task('watch', () => {
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass')); 
+  gulp.watch('*.html').on('change', browserSync.reload); 
+  gulp.watch('src/js/**/*.js').on('change', browserSync.reload); 
+});
+
+
 gulp.task('default', gulp.parallel('styles', 'scripts'));
